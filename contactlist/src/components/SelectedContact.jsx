c2259caa3abdc70767dabfc048ab3f9265f42548
Nick Lorusso <ndlorusso@gmail.com>
2024-04-03 15:29:23 -0500

    commit

--- contactlist/src/components/SelectedContact.jsx
+++ contactlist/src/components/SelectedContact.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 
 const SelectedContact = ({ selectedContactId, setSelectedContactId }) => {
     const [contact, setContact] = useState(null);
+    console.log('SINGLE contact', contact);
     return (
         <>
         <h1>Hello World</h1>
